840c6088ad14cc111bb648b7ee2193a5c94aca12
Smile4Blitz <arnohalsberghe@outlook.com>
2025-02-22 21:09:09 +0000

    test: all traversal orders

--- trees-recap/apps/recap.cpp
+++ trees-recap/apps/recap.cpp
@@ -3,7 +3,8 @@
 
 #include "tree.hpp"
 
-int main() {
+int main()
+{
     Tree<int> tree;
 
     tree.add(9);
@@ -22,24 +23,36 @@ int main() {
     std::ofstream after_deletion("after_deletion.dot");
     after_deletion << tree;
 
-    const auto printer = [](const Node<int>* node){ std::cout << node->key << " "; };
+    const auto printer = [](const Node<int> *node)
+    { std::cout << node->key << " "; };
 
     std::cout << "in order = ";
     tree.traverse<inorder>(printer);
     std::cout << std::endl;
 
-    Node<int>* node = tree.find(2);
-    if (node != nullptr) {
+    std::cout << "pre order = ";
+    tree.traverse<preorder>(printer);
+    std::cout << std::endl;
+
+    std::cout << "post order = ";
+    tree.traverse<postorder>(printer);
+    std::cout << std::endl;
+
+    Node<int> *node = tree.find(2);
+    if (node != nullptr)
+    {
         std::cout << "Found node for key: " << node->key << std::endl;
     }
 
-    Node<int>* not_found = tree.find(21);
-    if (not_found == nullptr) {
+    Node<int> *not_found = tree.find(21);
+    if (not_found == nullptr)
+    {
         std::cout << "No node for key: " << 21 << std::endl;
     }
 
     int total = 0;
-    tree.traverse<inorder>([&total](const Node<int>* node){ total += node->key; });
+    tree.traverse<inorder>([&total](const Node<int> *node)
+                           { total += node->key; });
 
     std::cout << "sum = " << total << std::endl;
 
@@ -47,10 +60,11 @@ int main() {
     int high = 14;
     std::cout << "range(" << low << ", " << high << ") = [ ";
 
-    for (auto val : tree.range(low, high)) {
+    for (auto val : tree.range(low, high))
+    {
         std::cout << val << " ";
     }
     std::cout << "]" << std::endl;
 
-	return 0;
+    return 0;
 }
